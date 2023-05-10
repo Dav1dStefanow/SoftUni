add45a0dc6ed52a66356a230751f5a904d8ffcf7
Dav1dStefanow <david.sstefanov@gmail.com>
2023-05-10 12:26:39 +0300

    Streams, Files and Directories Exercises

---
+++ C# Advanced/04. Streams, Files and Directories/Exercises/P01.EvenLines/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace P01.EvenLines
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileName = @"D:\SoftUniStreams\input.txt";
+            string pattern = @"[-,.!?]";
+
+            using StreamReader streamReader = new StreamReader(fileName);
+
+            string line = streamReader.ReadLine();
+            int counter = 0;
+
+
+            while (line != null)
+            {
+                if (counter % 2 == 0)
+                {
+                    foreach (Match m in Regex.Matches(line, pattern))
+                    {
+                        line = line.Replace(m.ToString(), "@");
+                    }
+
+                    string[] words = line.Split().ToArray();
+
+                    Console.WriteLine(string.Join(" ", words.Reverse()));
+                }
+
+                counter++;
+                line = streamReader.ReadLine();
+            }
+            
+
+        }
+    }
+}
---
+++ C# Advanced/04. Streams, Files and Directories/Exercises/P02.LineNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace P02.LineNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = @"D:\SoftUniStreams\input.txt";
+            string output = @"D:\SoftUniStreams\output.txt";
+            string pattern = @"[-,.!?]";
+            string patternn = @"[A-Za-z]";
+
+            using StreamReader sr = new StreamReader(input);
+            using StreamWriter sw = new StreamWriter(output);
+
+            string line = sr.ReadLine();
+            int counter = 1;
+            while(line != null)
+            {
+                int punkMarks = 0;
+                int lettersCount = 0;
+                foreach (Match m in Regex.Matches(line, pattern))
+                {
+                    punkMarks++;
+                }
+                foreach (Match m in Regex.Matches(line, patternn))
+                {
+                    lettersCount++;
+                }
+                sw.WriteLine($"Line {counter}: {line} ({punkMarks}) ({lettersCount})");
+                counter++;
+                line = sr.ReadLine();
+            }
+        }
+    }
+}
