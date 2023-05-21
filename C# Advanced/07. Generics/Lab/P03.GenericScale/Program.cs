f73061f5c93d5762f6ef574cb23f0073638ff074
Dav1dStefanow <david.sstefanov@gmail.com>
2023-05-21 10:08:05 +0300

    Update Program.cs

--- C# Advanced/07. Generics/Lab/P03.GenericScale/Program.cs
+++ C# Advanced/07. Generics/Lab/P03.GenericScale/Program.cs
@@ -7,8 +7,7 @@ namespace P03.GenericScale
         static void Main(string[] args)
         {
             string[] leftRight = Console.ReadLine().Split();
-            string left = leftRight[0];
-            string right = leftRight[1];
+            string left = leftRight[0]; string right = leftRight[1];
             Console.WriteLine(EqualityScale.AreEqual(left, right));
         }
     }
