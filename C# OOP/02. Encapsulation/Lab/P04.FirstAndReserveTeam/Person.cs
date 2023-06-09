868bcb084397fda982ae75802356c0ac6c3af75d
Dav1dStefanow <david.sstefanov@gmail.com>
2023-06-09 10:28:16 +0300

    Encapsulation Exercise

---
+++ C# OOP/02. Encapsulation/Lab/P04.FirstAndReserveTeam/Person.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04.FirstAndReserveTeam
+{
+    internal class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+            this.Name = firstName + " " + lastName;
+        }
+        private string name;
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            private set
+            {
+
+                this.firstName = value;
+                
+            }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            private set
+            {
+
+                this.lastName = value;
+                
+            }
+        }
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            private set
+            {
+
+
+                this.age = value;
+                
+            }
+        }
+        public decimal Salary
+        {
+            get { return this.salary; }
+            private set
+            {
+
+                this.salary = value;
+                
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age > 30)
+            {
+                this.Salary += this.Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += this.Salary * percentage / 200;
+            }
+        }
+        public override string ToString()
+        {
+            return $"{Name} recieves {Salary:F2} leva.";
+        }
+    }
+}
---
+++ C# OOP/02. Encapsulation/Lab/P04.FirstAndReserveTeam/Team.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04.FirstAndReserveTeam
+{
+    internal class Team
+    {
+        public Team(string name) 
+        { 
+            Name = name;
+        }
+        private string name;
+        private List<Person> firstTeam = new List<Person>();
+        private List<Person> reserveTeam = new List<Person>();
+
+        public string Name 
+        {
+            get { return name; } 
+            private set { name = value; }
+        }
+        public IReadOnlyList<Person> FirstTeam => this.firstTeam;
+        public IReadOnlyList<Person> ReserveTeam => this.reserveTeam;
+
+        public void AddPlayer(Person person)
+        {
+            if(person.Age < 40)
+            {
+                firstTeam.Add(person);
+            }
+            else
+            {
+                reserveTeam.Add(person);
+            }
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"First team has {FirstTeam.Count} players.");
+            sb.AppendLine($"Reserve team has {ReserveTeam.Count} players.");
+            return sb.ToString();
+        }
+    }
+}
